36912891512ea67e5b43f025190d52b2b8f631fc
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-10-02 19:40:05 +0200

    zadatak graphql po mateju

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 import "./App.css";
-import FrontPage from "./pages/FrontPage";
+import { gql, request } from "graphql-request";
 
 function App() {
+    const [countries, setCountries] = useState([]);
+
+    const query = gql`
+        {
+            countries {
+                currency
+                code
+                name
+                emoji
+            }
+        }
+    `;
+
+    const getCountries = async () => {
+        try {
+            const countriesData = await request(
+                "https://countries.trevorblades.com/graphql",
+                query
+            );
+            setCountries(countriesData.countries)
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <>
-            <FrontPage />
+            <button onClick={getCountries}>Get countries</button>
+            {
+                countries.map(country=> {
+                    return (
+                        <div key={country.code}>In {country.name}{country.emoji} the currency is {country.currency}</div>
+                    )
+                })
+            }
         </>
     );
 }
