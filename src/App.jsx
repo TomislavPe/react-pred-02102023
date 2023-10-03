3f27b0151cf59460421818bd3423b0ff7af5b4eb
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-10-03 18:29:09 +0200

    graphql primjer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-// import React from "react";
 import "./App.css";
-import NasaImage from "./NasaImage";
+import { gql, request } from "graphql-request";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <header>
-        <NasaImage />
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([]);
+
+    const query = gql`
+        {
+            countries(filter: {code: {eq: "HR"}}) {
+                code
+                currency
+                name
+                emoji
+                continent {
+                    name
+                }
+            }
+        }
+    `;
+
+    const getCountries = async () => {
+        try {
+            const countriesData = await request(
+                "https://countries.trevorblades.com/graphql",
+                query
+            );
+            console.log(countriesData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <button onClick={getCountries}>Get countries</button>
+        </>
+    );
 }
 
 export default App;
